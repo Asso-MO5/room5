bb61d57c497f7ab91270c38a554b414200dda3f5
Kazerlelutin <b@bouteiller.contact>
2023-10-16 01:06:56 +0200

    add end room

--- room5/data/sprt_player.h
+++ room5/data/sprt_player.h
@@ -2,7 +2,7 @@
 // ██  ▀  █▄  ▀██▄ ▀ ▄█ ▄  ▄█▄█ ▄▀██
 // █  █ █  ▀▀  ▄█  █  █ ██ ██ █  ▀██
 // ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀          ▀▀
-// Data generated using MSXimg 1.16.5 on Sun Oct 15 21:38:44 2023
+// Data generated using MSXimg 1.16.5 on Mon Oct 16 01:06:24 2023
 // by Guillaume "Aoineko" Blanchard (2022) under CC BY-SA free license
 // ─────────────────────────────────────────────────────────────────────────────
 // Generation parameters:

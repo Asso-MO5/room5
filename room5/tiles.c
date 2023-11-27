936fb04f9594e044cd0b3d89fb31cd6adf81a0d0
Sylvain Glaize <mokona@puupuu.org>
2023-11-27 22:28:20 +0100

    Commentaires dans tiles.c/.h

--- room5/tiles.c
+++ room5/tiles.c
@@ -18,13 +18,12 @@ u8 getTileByTileCoord(u8 x, u8 y)
 
 //-----------------------------------------------------------------------------
 // Modifie la tuile à la position indiquée
+//-----------------------------------------------------------------------------
 void setTile(u8 x, u8 y, u8 tile)
 {
 	VDP_Poke_16K(tile, g_ScreenLayoutLow + (y / 8) * 32 + (x / 8));
 }
 
-//-----------------------------------------------------------------------------
-// remplace la tuile à la position indiquée par une tuile vide avec les coordonnées de la tuile
 void setTileByTileCoord(u8 x, u8 y, u8 tile)
 {
 	VDP_Poke_16K(tile, g_ScreenLayoutLow + y * 32 + x);
@@ -32,6 +31,7 @@ void setTileByTileCoord(u8 x, u8 y, u8 tile)
 
 //-----------------------------------------------------------------------------
 // Test la collision à la position indiquée
+//-----------------------------------------------------------------------------
 bool checkCollision(u8 x, u8 y)
 {
 	return getTile(x, y) >= 128;
--- room5/tiles.h
+++ room5/tiles.h
@@ -1,9 +1,18 @@
 #pragma once
 #include "core.h"
 
-u8 getTile(u8 x, u8 y);
-u8 getTileByTileCoord(u8 x, u8 y);
+/*
+	 Interface qui permet de manipuler les tuiles à l'écran.
+*/
 
-void setTile(u8 x, u8 y, u8 tile);
+/* Récupère la tuile à la position indiquée */
+u8 getTile(u8 x, u8 y); // Coordonnées en pixels
+u8 getTileByTileCoord(u8 x, u8 y); // Coordonnées en tuiles
+
+/* Modifie la tuile à la position indiquée */
+void setTile(u8 x, u8 y, u8 tile); // Coordonnées en pixels
+void setTileByTileCoord(u8 x, u8 y, u8 tile); // Coordonnées en tuiles
+
+/* Test si la tuile à la position indiquée provoque une collision
+   Coordonnées en pixels */
 bool checkCollision(u8 x, u8 y);
-void setTileByTileCoord(u8 x, u8 y, u8 tile);

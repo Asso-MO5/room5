00c8fe280a412dc5d0b5a500ef5a593bea71eb92
Kazerlelutin <b@bouteiller.contact>
2024-04-28 23:01:31 +0200

    suppression vieux code chelou qui fait des trucs mais on sait pas quoi

--- room5/elevator.h
+++ room5/elevator.h
@@ -11,11 +11,10 @@ void addElevator(u8 x, u8 y);
 void updateAllElevators();
 void changeAllElevatorsColor(u8 color);
 void hideAllElevators();
-bool isOnElevator(u8 * X, u8 * Y);
+bool isOnElevator(u8 *X, u8 *Y);
 
 // Fonctions de gestion des élévateurs manuels
 void addManualElevator(u8 x, u8 y);
 void moveAllManualElevators(u8 tile);
-bool isManualElevatorAt(u8 x, u8 y); // Coordonnées en pixels
 
 bool checkRails(u8 x, u8 y);

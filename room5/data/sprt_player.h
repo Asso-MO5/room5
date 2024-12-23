bb139b1ba1bbde695515d015a745830cf324d36e
Kazerlelutin <b@bouteiller.contact>
2024-12-23 01:19:06 +0100

    nouvelles boites à fusible - inventaire.

--- room5/data/sprt_player.h
+++ room5/data/sprt_player.h
@@ -2,7 +2,7 @@
 // ██  ▀  █▄  ▀██▄ ▀ ▄█ ▄  ▄█▄█ ▄▀██
 // █  █ █  ▀▀  ▄█  █  █ ██ ██ █  ▀██
 // ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀          ▀▀
-// Data generated using MSXimg 1.20.1 on Sun Dec 22 16:44:11 2024
+// Data generated using MSXimg 1.20.0 on Mon Dec 23 01:16:03 2024
 // by Guillaume "Aoineko" Blanchard (2022) under CC BY-SA free license
 // ─────────────────────────────────────────────────────────────────────────────
 // Generation parameters:
